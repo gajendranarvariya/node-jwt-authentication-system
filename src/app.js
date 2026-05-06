0540eb0c8749a263705f48def6ac408ddb60a501
Gajendra <gajendra88.csl@gmail.com>
2026-05-06 14:48:58 +0530

    add more security feature and add some middleware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
 
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
+ const limiter = rateLimit({
+    windowMs : 15*60*1000 // 15 minute // time set in milisecond
+    max: 100, // 
+    message: "Too many rquests from this IP, please try again later" //
+});
+
 
 // use middleware body parser
-app.use(express.json());
+app.use(express.json({limit:"20kb"}));
+app.use(express.urlencoded({ limit: "20kb", extended: true }));
 app.use(cookieParser());
 
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
+app.use(helmet());
+app.use(limiter);
 app.use("/api/auth", authRouter);
+
+
+app.use((err, req, res, next) => {
+  if (err.type === "entity.too.large") {
+    return res.status(413).json({
+      error: "Request body too large"
+    });
+  }
+  return res.status(500).json({
+      error: "Something went wrong"
+    });
+  next(err);
+});
 
 
 export default app;
