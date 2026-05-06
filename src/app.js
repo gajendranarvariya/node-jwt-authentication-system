b0a7b4a1f131e0b735e7e3bc953e8e31dd858471
Gajendra <gajendra88.csl@gmail.com>
2026-05-06 14:10:57 +0530

    add some package this file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
+
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
+
+// use middleware body parser
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.use("/api/auth", authRouter);
 
 
 export default app;
+
